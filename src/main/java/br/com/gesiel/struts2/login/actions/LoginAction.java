33f9fb3f2cc83561df6175652d2a63ee9caf6760
Gesiel Diniz da Silva <gesiel.diniz@gmail.com>
2018-11-26 18:08:32 -0200

    Upload projeto

---
+++ src/main/java/br/com/gesiel/struts2/login/actions/LoginAction.java
@@ -0,0 +1,29 @@
+package br.com.gesiel.struts2.login.actions;
+
+public class LoginAction {
+	
+	private String email;
+	private String senha;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+	public String logar(){
+		if(this.email.equals("teste@teste.com") && this.senha.equals("123")){
+			return "SUCCESS";
+		}else{
+			return "ERROR";
+		}
+	}
+}
